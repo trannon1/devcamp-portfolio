960dd1eea42759f14bfbbec9a0bf473917e1f850
Bryan Tran <tran_b1@denison.edu>
2018-07-09 18:30:20 -0400

    Integrated skills via model-generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_09_214549) do
+ActiveRecord::Schema.define(version: 2018_07_09_221951) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
