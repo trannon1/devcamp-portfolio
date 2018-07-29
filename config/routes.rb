9c64aa056634fac1d9e9edf0953474168ebd2093
Bryan Tran <tran_b1@denison.edu>
2018-07-29 12:52:42 -0400

    Implemented form configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :portfolios, except: [:show]
+  resources :portfolios
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
