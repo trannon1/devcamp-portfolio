6bca61d336b432ff1b4b007ef037fe683251104b
Bryan Tran <tran_b1@denison.edu>
2018-07-30 10:51:43 -0700

    Added Guest User

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,11 @@
+module CurrentUserConcern
+    extend ActiveSupport::Concern
+
+    def current_user
+        super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
+    end
+
+    def guest_user
+        OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
+    end
+end
