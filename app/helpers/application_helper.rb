12218497dac098196282daf7ab3566d6b40a1b8c
Bryan Tran <tran_b1@denison.edu>
2018-08-06 20:25:02 -0700

    Implemented redis

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+    DevcampViewTool::Renderer.copyright 'Bryan Tran', 'All rights reserved'
   end
 
   def nav_items
@@ -68,7 +68,7 @@ module ApplicationHelper
   end
 
   def alert_generator msg
-    js add_gritter(msg, title: "Jordan Hudgens Portfolio", sticky: false)
+    js add_gritter(msg, title: "Bryan Tran Portfolio", sticky: false)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
 
   resources :blogs do
     member do
